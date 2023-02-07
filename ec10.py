7939a8a8d1d5db31e0d218e1af7f2719860317da
Vusi Dube <vusi.dube@absa.africa>
2023-02-07 21:28:47 +0200

    Updated for api.easycrypto.co.za
    - new json format
    - added ece10 and eca20 pricing

--- ec10.py
+++ ec10.py
@@ -1,10 +1,23 @@
 import requests
 import datetime
+from pycoingecko import CoinGeckoAPI
+import json
 
-api_url="https://easycrypto.co.za/api/Currency/GetDCXTotalBalance"
+ec_api_url="https://api.easycrypto.co.za/api/v2/GetPrice"
 requests.packages.urllib3.disable_warnings()
-response = requests.get(api_url, verify=False)
+response = requests.get(ec_api_url, verify=False)
 data = response.json()
 
 now = datetime.datetime.now()
-print(now.strftime("%Y-%m-%d %H:%M:%S")+ " EC10 price R%0.2f"%(data["TokenZarPrice"]))
+print(now.strftime("%Y-%m-%d %H:%M:%S"))
+
+for i in data:
+    if i['Symbol'] == 'EC10' or i['Symbol'] == 'ECE10' or i['Symbol'] == 'ECA20':
+        print("%s R%0.2f" % (i['Symbol'], i['Zar']['BidPrice']))
+
+cg = CoinGeckoAPI()
+coin_prices = cg.get_price(ids='bitcoin,ethereum,cardano,binance,solana,terra-luna', vs_currencies='usd')
+
+
+for coin, price in coin_prices.items():
+    print('%s \t $%s' % (coin.capitalize(), price['usd']))
