37257e383926aa42d83a3084c77f7850ec1f29b2
Vusi <vusadube@gmail.com>
2023-03-01 07:27:47 +0200

    Used logging module to priint errorrs

--- ec10.py
+++ ec10.py
@@ -1,22 +1,25 @@
 import requests
 import datetime
+import logging
 from pycoingecko import CoinGeckoAPI
+
+log = logging.getLogger(__name__)
 
 ec_api_url="https://api.easycrypto.co.za/api/v2/GetPrice"
 requests.packages.urllib3.disable_warnings()
+
+now = datetime.datetime.now()
+print(now.strftime("%Y-%m-%d %H:%M:%S"))
 
 try:
     response = requests.get(ec_api_url, verify=False)
     data = response.json()
 
-    now = datetime.datetime.now()
-    print(now.strftime("%Y-%m-%d %H:%M:%S"))
-
     symbols = ['EC10', 'ECE10', 'ECA20', 'ECNMG']
     for crypto in [i for i in data if i['Symbol'] in symbols]:
         print("%s R%0.2f" % (crypto['Symbol'], crypto['Zar']['BidPrice']))
 except requests.exceptions.ConnectionError as ce:
-    print("Connection error calling  EasyCrypto url %s" % ec_api_url )
+    log.error(f"Connection error calling  EasyCrypto url {ce.request.url}")
 
 # Use CoinGecko api
 cg = CoinGeckoAPI()
@@ -24,6 +27,6 @@ try:
     coin_prices = cg.get_price(ids='bitcoin,ethereum,cardano,binance,solana,terra-luna', vs_currencies='usd')
 
     for coin, price in coin_prices.items():
-        print('%s \t $%s' % (coin.capitalize(), price['usd']))
+        print("%s \t $%s" % (coin.capitalize(), price['usd']))
 except requests.exceptions.ConnectionError as ce:
-    print("Connection error calling  CoinGecko url %s" % ce.request.url )
+    log.error(f"Connection error calling CoinGecko url {ce.request.url}")
